f6a0682c99251369a70041bd433b74544f7cdef5
Emmanuel Ubhimhiye <76892228+eubhimhiye@users.noreply.github.com>
2024-03-11 23:22:59 -0400

    Update eks.tf

--- eks.tf
+++ eks.tf
@@ -24,11 +24,12 @@ resource "aws_eks_cluster" "eks_cluster" {
     aws_iam_role_policy_attachment.eks-AmazonEKSClusterPolicy,
     aws_iam_role_policy_attachment.eks-AmazonEKSVPCResourceController,
   ]
+/*
   encryption_config {
     resources = [ secrets ]
     provider {
       key_arn = true
     }
   }
- 
+ */
 }
